2e10400ec3c648f51f0b4d7b1e8a9db7e92080a8
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-22 23:21:56 +0200

    1

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -11,4 +11,4 @@ sed -i "77c $puerto" /home/pi/status.ini
 x=$(awk "NR==93" /home/pi/status.ini)
 
 cd /home/pi/MMDVMHost
-xterm -geometry 76x10+$x+656  -bg black -fg cyan -fa 'serift' -fs 10x -T DMO -e sudo ./MMDVMHost MMDVM.ini &
+xterm -geometry 76x9+$x+640  -bg black -fg cyan -fa 'serift' -fs 10x -T DMO -e sudo ./MMDVMHost MMDVM.ini &
