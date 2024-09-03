e9fdcfc6dbdbfc93085f18affd7f32ed306272fb
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-09-03 21:20:15 +0200

    Update ejecutar_DMR2YSF.sh

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -9,13 +9,13 @@ puerto=`expr substr $puerto 15 14`
 sed -i "83c $puerto" /home/pi/status.ini
 
 cd /home/pi/DMR2YSF
-xterm -geometry 88x6+1274+165 -bg black -fg yellow -fa 'serift' -fs 9x -T DMR2YSF -e sudo ./DMR2YSF DMR2YSF.ini &
+xterm -geometry 88x6+1274+165 -bg black -fg yellow -fa 'serift' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
 
 cd /home/pi/MMDVMHost
 xterm -geometry 87x5+1287+832 -bg black -fg yellow -fa 'serift' -fs 9x -T MMDVMDMR2YSF -e sudo ./DMR2YSF MMDVMDMR2YSF.ini &
 
 cd /home/pi/YSFClients/YSFGateway
-xterm -geometry 87x5+1287+943 -bg black -fg yellow -fa 'serift' -fs 9x -T YSFGateway -e sudo ./YSFGateway YSFGateway.ini &
+xterm -geometry 87x5+1287+943 -bg black -fg yellow -fa 'serift' -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini &
 
 
 
