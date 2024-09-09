58738c0a6632ecd7193422a6e39aaad71bb8adc0
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-09-09 17:32:40 +0200

    Update ejecutar_DMR2NXDN.sh

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -9,11 +9,10 @@ puerto=`expr substr $puerto 15 14`
 sed -i "93c $puerto" /home/pi/status.ini
 
 cd /home/pi/DMR2NXDN
-xterm -geometry 88x6+1274+665 -bg violet -fg black -fa ‘verdana’ -fs 9x -T CONSOLA_DMR2NXDN -e ./DMR2NXDN DMR2NXDN.ini & 
+xterm -geometry 88x6+1274+665 -bg violet -fg black -fa ‘verdana’ -fs 9x -T DMR2NXDN -e ./DMR2NXDN DMR2NXDN.ini & 
 cd /home/pi/MMDVMHost
-#/home/pi/SYSTEM/./qt_info_dmr2nxdn & sudo lxterminal --geometry=75x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
-xterm -geometry 88x9+1274+787 -bg violet -fg black -fa ‘verdana’ -fs 9x -T CONSOLA_MMDVMDMR2NXDN -e ./DMR2NXDN MMDVMDMR2NXDN.ini & 
+xterm -geometry 88x9+1274+787 -bg violet -fg black -fa ‘verdana’ -fs 9x -T MMDVMDMR2NXDN -e ./DMR2NXDN MMDVMDMR2NXDN.ini & 
 cd /home/pi/NXDNClients/NXDNGateway
-xterm -geometry 88x4+1274+0 -bg violet -fg black -fa ‘verdana’ -fs 9x -T CONSOLA_NXDNGateway -e ./NXDNGateway NXDNGateway.ini
+xterm -geometry 88x4+1274+0 -bg violet -fg black -fa ‘verdana’ -fs 9x -T NXDNGateway -e ./NXDNGateway NXDNGateway.ini
 
 
