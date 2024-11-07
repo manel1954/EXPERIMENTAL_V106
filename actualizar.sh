984e5b6ccd5535db8ea6af9d0f172f39c649a7f5
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-07 15:50:25 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -5,7 +5,7 @@ if [ "$dvswitch" = 'DVSWITCH=OFF' ];then
 sudo systemctl stop ysfgateway.service
 sudo systemctl stop dmr2ysf.service
 sudo systemctl stop analog_bridge.service
-sudo systemctl stop ircddbgateway.service
+sudo systemctl stop ircddbgatewayd.service
 sudo systemctl stop md380-emu.service
 sudo systemctl stop mmdvm_bridge.service
 sudo systemctl stop nxdngateway.service
