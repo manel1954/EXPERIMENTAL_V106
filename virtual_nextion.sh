d792f0c6b8adfa849a0e30090ac5387781e0738a
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-11 17:49:50 +0100

    Update virtual_nextion.sh

--- virtual_nextion.sh
+++ virtual_nextion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # sudo killall socat  
 cd /home/pi/A108
-#sudo socat -d -d pty,link=/dev/virtual1,raw,echo=0 pty,link=/dev/virtual2,raw,echo=0 &
+sudo socat -d -d pty,link=/dev/virtual1,raw,echo=0 pty,link=/dev/virtual2,raw,echo=0 &
 
-sudo socat -d -d PTY,raw,echo=0,b9600,link=/dev/virtual1 PTY,raw,echo=0,b9600,link=/dev/virtual2
+#sudo socat -d -d PTY,raw,echo=0,b9600,link=/dev/virtual1 PTY,raw,echo=0,b9600,link=/dev/virtual2
 
 sudo python3 nextion_5.py &
