4970c3dab84b0f20af9ce5a2d1c8b5e5423e079f
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-16 00:57:27 +0100

    Update virtual_nextion_1.py

--- virtual_nextion_1.py
+++ virtual_nextion_1.py
@@ -43,7 +43,7 @@ LABEL_CONFIGS = {
     "IP": {"fg": "white", "font": ("Arial", 12, "bold"), "row": 3, "column": 0},
     "Estado": {"fg": "white", "font": ("Arial", 12, "bold"), "row": 3, "column": 1},
     "Ber": {"fg": "yellow", "font": ("Arial", 12, "bold"), "row": 4, "column": 0},
-    "LH": {"fg": "yellow", "font": ("Arial", 12, "bold"), "row": 0, "column": 0},
+    "LH": {"fg": "yellow", "font": ("Arial", 12, "bold"), "row": 4, "column": 0},
     "RSSI": {"fg": "yellow", "font": ("Arial", 12, "bold"), "row": 4, "column": 1},
     "Temp": {"fg": "#ff5722", "font": ("Arial", 10, "bold"), "row": 5, "column": 0},
     "TG": {"fg": "#00adb5", "font": ("Arial", 10, "bold"), "row": 5, "column": 1},
@@ -70,7 +70,7 @@ estacion_label = tk.Label(
     text="", 
     bg=WINDOW_BG_COLOR, 
     fg="#00adb5", 
-    font=("Arial", 16, "bold"),
+    font=("Arial", 10, "bold"),
     highlightbackground="#1E90FF",  # Borde azul
     highlightthickness=2          # Grosor del borde
 )
