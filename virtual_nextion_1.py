2a90ed038f1a1b62755b28cd6b3e8b6ec97a7e11
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-16 00:25:34 +0100

    Update virtual_nextion_1.py

--- virtual_nextion_1.py
+++ virtual_nextion_1.py
@@ -40,7 +40,7 @@ root.rowconfigure(0, weight=0)
 LABEL_CONFIGS = {
     "Frecuencia RX": {"fg": "#77DD77", "font": ("Arial", 12, "bold"), "row": 2, "column": 1},
     "Frecuencia TX": {"fg": "pink", "font": ("Arial", 12, "bold"), "row": 2, "column": 2},
-    "IP": {"fg": "white", "font": ("Arial", 12, "bold"), "row": 3, "column": 0},
+    "IP": {"fg": "white", "font": ("Arial", 12, "bold"), "row": 3, "column": 1},
     "Estado": {"fg": "white", "font": ("Arial", 12, "bold"), "row": 3, "column": 1},
     "Ber": {"fg": "yellow", "font": ("Arial", 12, "bold"), "row": 4, "column": 0},
     "LH": {"fg": "yellow", "font": ("Arial", 12, "bold"), "row": 4, "column": 2},
