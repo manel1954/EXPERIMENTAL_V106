38e4ae94f8d69c077f2f7a041f961fc6f23379d0
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-18 17:48:07 +0100

    1

--- desactivar_dvswitch.sh
+++ desactivar_dvswitch.sh
@@ -7,5 +7,4 @@ sudo systemctl stop md380-emu.service
 sudo systemctl stop mmdvm_bridge.service
 sudo systemctl stop nxdngateway.service
 sudo systemctl stop p25gateway.service
-sudo killall ircddbgatewayd
 sed -i "18c DVSWITCH=OFF" /home/pi/status.ini
