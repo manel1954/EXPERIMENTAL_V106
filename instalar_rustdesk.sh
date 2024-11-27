1d8a7268f0e8e5923827da3bad4b970c51faf519
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-27 13:15:33 +0100

    Update instalar_rustdesk.sh

--- instalar_rustdesk.sh
+++ instalar_rustdesk.sh
@@ -4,4 +4,4 @@
                         
                         wget https://github.com/rustdesk/rustdesk/releases/download/1.3.2/rustdesk-1.3.2-armv7-sciter.deb
                         
-                        sudo dpkg -i rustdesk-1.3.2-x86_64-sciter.deb
+                        sudo dpkg -i rustdesk-1.3.2-armv7-sciter.deb
