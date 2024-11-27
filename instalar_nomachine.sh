e17cf6608504c8ca468f984f9f9508d574091620
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-27 16:15:21 +0100

    1

---
+++ instalar_nomachine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ 
+                        cd /home/pi/Downloads                       
+
+                        wget https://downloads.nomachine.com/es/download/nomachine_8.14.2_1_armhf.deb 
+                        
+                        sudo dpkg -i nomachine_8.14.2_1_armhf.deb
