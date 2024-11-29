e84dc19757cd90bbf46d1fc8406c89d06e1a7d0f
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-29 22:01:02 +0100

    1

--- activar_dvswitch.sh
+++ activar_dvswitch.sh
@@ -3,6 +3,7 @@
 sudo systemctl restart dmr2ysf.service
 sudo systemctl restart analog_bridge.service
 sudo systemctl restart ircddbgatewayd.service
+sudo systemctl restart ircddbgateway.service
 sudo systemctl restart md380-emu.service
 sudo systemctl restart mmdvm_bridge.service
 sudo systemctl restart nxdngateway.service
--- desactivar_dvswitch.sh
+++ desactivar_dvswitch.sh
@@ -3,6 +3,7 @@
 sudo systemctl stop dmr2ysf.service
 sudo systemctl stop analog_bridge.service
 sudo systemctl stop ircddbgatewayd.service
+sudo systemctl stop ircddbgateway.service
 sudo systemctl stop md380-emu.service
 sudo systemctl stop mmdvm_bridge.service
 sudo systemctl stop nxdngateway.service
