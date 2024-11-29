836e49a50d1636e8b89be362c42b71dc65569386
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-29 10:38:47 +0100

    1

--- instalar_rustdesk.sh
+++ instalar_rustdesk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
  
+                        sudo rm -R /home/pi/.config/rustdesk
+                        
                         cd /home/pi/Downloads
                         
                         wget https://github.com/rustdesk/rustdesk/releases/download/1.3.2/rustdesk-1.3.2-armv7-sciter.deb
@@ -9,3 +11,4 @@
                         sudo apt -f install -y
 
                         sudo rm rustdesk-1.3.2-armv7-sciter.deb*
+
