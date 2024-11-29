d219043f6766d270a361f9b129a97141740cf995
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-29 20:11:45 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+fecha=$(date +%Y%m%d) # extrae fecha del día en formato 20241129
+# Convertir a formato dd-mm-yyyy 29-11-2024
+fecha_formateada=$(echo "$fecha" | awk '{print substr($0, 7, 2) "-" substr($0, 5, 2) "-" substr($0, 1, 4)}')
+sed -i "2c $fecha_formateadaassword" /home/pi/version-fecha-actualizacion
+
                         cd /home/pi/EXPERIMENTAL_V106
-                        #git clone http://github.com/manel1954/EXPERIMENTAL_V106                       
+                        #git clone http://github.com/manel1954/EXPERIMENTAL_V106                        
                         git pull --force  
                         #cd /home/pi/A108/qt/
                         #./qt_popus_actualizada &                    
