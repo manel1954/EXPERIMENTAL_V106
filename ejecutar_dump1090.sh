c5999c7050ca1c85f215af984ebe8c88de5547e0
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-12-02 18:53:26 +0100

    Update ejecutar_dump1090.sh

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -15,9 +15,9 @@ if [ "$stick" = 'RSP1' ];then
 xterm -geometry 73x18+18+52 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/pi/dump1090_sdrplay/dump1090 --net --interactive --gain $gain --dev-sdrplay
 
 elif [ "$gain" = '-10' ];then
-xterm -geometry 73x18+8+60 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/pi/dump1090/dump1090 --device $index --net --interactive --net-ro-port $raw --net-bo-port $beast --gain $gain --ppm $ppm --net-http-port $http
+xterm -geometry 73x17+8+62 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/pi/dump1090/dump1090 --device $index --net --interactive --net-ro-port $raw --net-bo-port $beast --gain $gain --ppm $ppm --net-http-port $http
 else
-xterm -geometry 73x18+8+60 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/pi/dump1090/dump1090 --device $index --net --interactive --net-ro-port $raw --net-bo-port $beast --ppm $ppm --net-http-port $http
+xterm -geometry 73x17+8+62 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/pi/dump1090/dump1090 --device $index --net --interactive --net-ro-port $raw --net-bo-port $beast --ppm $ppm --net-http-port $http
 fi
 
 #sed -i "6c Exec=sh ejecutar_dump1090.sh" /home/pi/Desktop/dump1090.desktop
