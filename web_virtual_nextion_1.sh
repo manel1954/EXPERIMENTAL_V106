1ac5157191be46ffcf8153455dc334dd5103982d
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-12-28 12:39:46 +0100

    1

--- web_virtual_nextion_1.sh
+++ web_virtual_nextion_1.sh
@@ -2,4 +2,4 @@
 # sudo killall socat  
 cd /home/pi/A108
 sudo socat -d -d pty,link=/dev/virtual1,raw,echo=0 pty,link=/dev/virtual10,raw,echo=0 &
-sudo python3 app.py
+sudo python3 web_virtual_nextion_1.py
--- web_virtual_nextion_2.sh
+++ web_virtual_nextion_2.sh
@@ -2,4 +2,4 @@
 # sudo killall socat  
 cd /home/pi/A108
 sudo socat -d -d pty,link=/dev/virtual2,raw,echo=0 pty,link=/dev/virtual20,raw,echo=0 &
-sudo python3 nextion_web_2.py
+sudo python3 web_virtual_nextion_2.py
